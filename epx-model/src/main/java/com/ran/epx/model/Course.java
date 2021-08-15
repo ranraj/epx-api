bd3b17f77119452caec42dfbcd3dbcdc0077a2d0
ranjithraj.d <randhana@cisco.com>
2021-08-15 20:59:40 +0530

    Course domain altered for published

--- epx-model/src/main/java/com/ran/epx/model/Course.java
+++ epx-model/src/main/java/com/ran/epx/model/Course.java
@@ -1,8 +1,13 @@
 package com.ran.epx.model;
 
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -11,17 +16,28 @@ import lombok.experimental.Accessors;
 @Accessors
 public class Course {
 	@Id
-    private String id;
-    private String ownerId;
-    private String title;   
-    private String name;
-    private String photo;
-    private List<String> tags;    
-    private String category;
-    private double cost;
-    private int rating;
-    private String description;    
-    private List<String> likedBy;  
-    private float version;
-    private boolean deleted;
+	private String id;	
+	private String title;
+	private String name;
+	private String photo;
+	private List<String> tags;
+	private String category;
+	private double cost;
+	private int rating;
+	private String description;
+	private List<String> likedBy;
+	private float version;
+	private boolean deleted;
+	private boolean published;
+
+	@CreatedBy
+	private String ownerId;
+	@LastModifiedBy
+	private String modifiedBy;
+	
+	@CreatedDate
+	private Date createdDate;
+	@LastModifiedDate
+	private Date lastModifiedDate;
+
 }
